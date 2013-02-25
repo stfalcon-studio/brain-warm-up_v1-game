c47d0759bd5158e007b56ec5c02413dc710d7d0d
ftrrtf <facktoreal@gmail.com>
2013-02-25 17:13:04 +0200

    Добавлены тестовые наборы в провайдер данных

--- tests/BaseGameTest.php
+++ tests/BaseGameTest.php
@@ -41,20 +41,114 @@ abstract class BaseGameTest extends \PHPUnit_Framework_TestCase
      */
     public static function provider()
     {
-        $data = [];
+        // $data[n] in CLI = Game::testWhoWillWin with data set #n
 
-        $data[] = [
-            ['aba'],
+        $data[0] = [
+            'a',
             self::WIN_FIRST
         ];
 
-        $data[] = [
-            ['abca'],
+        $data[1] = [
+            'ab',
             self::WIN_SECOND
+        ];
+
+        $data[2] = [
+            'zz',
+            self::WIN_FIRST
+        ];
+
+        $data[3] = [
+            'aba',
+            self::WIN_FIRST
+        ];
+
+        $data[4] = [
+            'abca',
+            self::WIN_SECOND
+        ];
+
+        $data[5] = [
+            'aabb',
+            self::WIN_FIRST
+        ];
+
+        $data[6] = [
+            'abacaba',
+            self::WIN_FIRST
+        ];
+
+        $data[7] = [
+            'abazaba',
+            self::WIN_FIRST
+        ];
+
+        $data[8] = [
+            'aassddxyz',
+            self::WIN_FIRST
+        ];
+
+        $data[9] = [
+            'aabc',
+            self::WIN_SECOND
+        ];
+
+        $data[10] = [
+            'abcabc',
+            self::WIN_FIRST
+        ];
+
+        $data[11] = [
+            'aaabbbccdd',
+            self::WIN_SECOND
+        ];
+
+        $data[12] = [
+            'aabbcccc',
+            self::WIN_FIRST
+        ];
+
+        $data[13] = [
+            'gevqgtaorjixsxnbcoybr',
+            self::WIN_FIRST
+        ];
+
+        $data[14] = [
+            'xvhtcbtouuddhylxhplgjxwlo',
+            self::WIN_FIRST
+        ];
+
+        $data[15] = [
+            'ctjxzuimsxnarlciuynqeoqmmbqtagszuo',
+            self::WIN_SECOND
+        ];
+
+        $data[16] = [
+            'abcdefghijklmnopqrstuvwxyz',
+            self::WIN_SECOND
+        ];
+
+        $data[17] = [
+            'desktciwoidfuswycratvovutcgjrcyzmilsmadzaegseetexygedzxdmorxzxgiqhcuppshcsjcozkopebegfmxzxxagzwoymlghgjexcgfojychyt',
+            self::WIN_FIRST
+        ];
+
+        $data[18] = [
+            'vnvtvnxjrtffdhrfvczzoyeokjabxcilmmsrhwuakghvuabcmfpmblyroodmhfivmhqoiqhapoglwaluewhqkunzitmvijaictjdncivccedfpaezcnpwemlohbhjjlqsonuclaumgbzjamsrhuzqdqtitygggsnruuccdtxkgbdd',
+            self::WIN_FIRST
+        ];
+
+        $data[19] = [
+            'hxueikegwnrctlciwguepdsgupguykrntbszeqzzbpdlouwnmqgzcxejidstxyxhdlnttnibxstduwiflouzfswfikdudkazoefawm',
+            self::WIN_SECOND
+        ];
+
+        $data[20] = [
+            'aaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbccccccccccccccccccccddddddddddeeeeeeeeeeffffgggghhhhiiiijjjjqqqqwwwweeeerrrrttttyyyyuuuuiiiiooooppppaaaassssddddffffgggghhhhjjjjkkkkllllzzzzxxxxccccvvvvbbbbnnnnmmmm',
+            self::WIN_FIRST
         ];
 
         return $data;
     }
-
 
 }
