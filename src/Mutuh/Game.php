81ab2464c93915c1de701e568e8a08207fe100b1
Mutuh <sirmutuh@gmail.com>
2013-03-04 17:49:12 +0200

    php 5.3 arrays

---
+++ src/Mutuh/Game.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mutuh;
+
+class Game {
+
+    public function whoWillWin($input)
+    {
+        /**
+         * Implementation
+         */
+
+        return 'First';
+    }
+
+}
---
+++ src/Mutuh/tests/GameTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mutuh;
+
+class GameTest extends \BaseGameTest {
+
+    protected function setUpGame()
+    {
+        $this->game = new Game();
+    }
+
+}
