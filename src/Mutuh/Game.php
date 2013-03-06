20a01c8b60705a6b653859e2e6e72784b3e76328
Mutuh <sirmutuh@gmail.com>
2013-03-06 11:31:54 +0200

    Commenting code

--- src/Mutuh/Game.php
+++ src/Mutuh/Game.php
@@ -8,43 +8,43 @@ class Game {
     {
         $reversedInput = strrev($input);
 
+        // if input value == reversed input value - we are done :)
         if($input == $reversedInput) {
             return 'First';
         } else {
 
-            $symbols = array();
+            $symbols = $this->assocArrayOfSymbols($input);
 
-            foreach (count_chars($input, 1) as $char => $amount) {
-                $symbols[chr($char)] = $amount;
-            }
-
+            // string symbols sorting by amount
             array_multisort($symbols, SORT_DESC);
 
             $palindrome = '';
 
-            $i = 0;
+            $symbolsCount = 0;
             $removedSymbolsCounter = 0;
-            $length = count($symbols);
+            $symbolsAmount = count($symbols);
             $notPairSymbols = '';
 
-            foreach($symbols as $key => $val) {
-                $i++;
+            foreach($symbols as $symbolValue => $currentSymbolAmount) {
+                $symbolsCount++;
 
-                if ($i != $length && $length > 1 && $val % 2 != 0) {
-
-                    for ($y = 0; $y < $val; $y++) {
-                        $notPairSymbols .= $key;
+                if ($symbolsCount != $symbolsAmount && $symbolsAmount > 1 && $currentSymbolAmount % 2 != 0) {
+                    // array of not pair symbols. i will use it later
+                    for ($notPairSymbolsCount = 0; $notPairSymbolsCount < $currentSymbolAmount; $notPairSymbolsCount++) {
+                        $notPairSymbols .= $symbolValue;
                     }
                     $removedSymbolsCounter++;
                 } else {
-                    if ($i == 1) {
-                        for ($y = 0; $y < $val; $y++) {
-                            $palindrome .= $key;
+                    // inserting pair symbols to a center of palindrome output string
+                    if ($symbolsCount == 1) {
+                        for ($notPairSymbolsCount = 0; $notPairSymbolsCount < $currentSymbolAmount; $notPairSymbolsCount++) {
+                            $palindrome .= $symbolValue;
                         }
                     } else {
                         $middle = strlen($palindrome) * 0.5;
-                        for ($y = 0; $y < $val; $y++) {
-                            $palindrome = substr_replace($palindrome, $key, $middle, 0);
+                        for ($notPairSymbolsCount = 0; $notPairSymbolsCount < $currentSymbolAmount; $notPairSymbolsCount++) {
+                            //inserting to a center of a string
+                            $palindrome = substr_replace($palindrome, $symbolValue, $middle, 0);
                         }
                     }
                 }
@@ -54,26 +54,24 @@ class Game {
 
             $palindromeLength = strlen($palindrome);
 
+            // if palindrome string has pair symbols, then we could insert values of the last unpair symbols in the center of a palyndrome string
+            // rest of unpair strings amout must be decreased by one
             if ($palindromeLength % 2 == 0) {
-                $symbols = array();
+                $symbols = $this->assocArrayOfSymbols($notPairSymbols);
 
-                foreach (count_chars($notPairSymbols, 1) as $char => $amount) {
-                    $symbols[chr($char)] = $amount;
-                }
+                $symbolsAmount = count($symbols);
+                $pairSymbolsCount = 0;
 
-                $length = count($symbols);
-                $z = 0;
-
-                foreach($symbols as $key => $val) {
-                    $z++;
-                    if ($z == $length) {
+                foreach($symbols as $symbolValue => $currentSymbolAmount) {
+                    $pairSymbolsCount++;
+                    if ($pairSymbolsCount == $symbolsAmount) {
                         $removedSymbolsCounter--;
                     } else {
-                        $val--;
+                        $currentSymbolAmount--;
                     }
                     $middle = strlen($palindrome) * 0.5;
-                    for ($y = 0; $y < $val; $y++) {
-                        $palindrome = substr_replace($palindrome, $key, $middle, 0);
+                    for ($notPairSymbolsCount = 0; $notPairSymbolsCount < $currentSymbolAmount; $notPairSymbolsCount++) {
+                        $palindrome = substr_replace($palindrome, $symbolValue, $middle, 0);
                     }
                 }
             }
@@ -84,6 +82,15 @@ class Game {
                 return 'Second';
             }
         }
+    }
+
+    private function assocArrayOfSymbols($inputString)
+    {
+        foreach (count_chars($inputString, 1) as $char => $amount) {
+            $symbols[chr($char)] = $amount;
+        }
+
+        return $symbols;
     }
 
 }
