c05d5a2f3bdb9f9759eb8df927693075d2d09a59
vatseek <vatseek13@rambler.ru>
2013-03-14 16:55:47 +0200

    fix tests

---
+++ src/vatseek/tests/GameTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace vatseek;
+
+class GameTest extends \BaseGameTest {
+
+    protected function setUpGame()
+    {
+        $this->game = new Game();
+    }
+
+}
--- tests/BaseGameTest.php
+++ tests/BaseGameTest.php
@@ -43,110 +43,110 @@ abstract class BaseGameTest extends \PHPUnit_Framework_TestCase
     {
         // $data[n] in CLI = Game::testWhoWillWin with data set #n
 
-        $data[0] = [
+        $data[0] = array(
             'a',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[1] = [
+        $data[1] = array(
             'ab',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[2] = [
+        $data[2] = array(
             'zz',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[3] = [
+        $data[3] = array(
             'aba',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[4] = [
+        $data[4] = array(
             'abca',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[5] = [
+        $data[5] = array(
             'aabb',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[6] = [
+        $data[6] = array(
             'abacaba',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[7] = [
+        $data[7] = array(
             'abazaba',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[8] = [
+        $data[8] = array(
             'aassddxyz',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[9] = [
+        $data[9] = array(
             'aabc',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[10] = [
+        $data[10] = array(
             'abcabc',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[11] = [
+        $data[11] = array(
             'aaabbbccdd',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[12] = [
+        $data[12] = array(
             'aabbcccc',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[13] = [
+        $data[13] = array(
             'gevqgtaorjixsxnbcoybr',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[14] = [
+        $data[14] = array(
             'xvhtcbtouuddhylxhplgjxwlo',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[15] = [
+        $data[15] = array(
             'ctjxzuimsxnarlciuynqeoqmmbqtagszuo',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[16] = [
+        $data[16] = array(
             'abcdefghijklmnopqrstuvwxyz',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[17] = [
+        $data[17] = array(
             'desktciwoidfuswycratvovutcgjrcyzmilsmadzaegseetexygedzxdmorxzxgiqhcuppshcsjcozkopebegfmxzxxagzwoymlghgjexcgfojychyt',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[18] = [
+        $data[18] = array(
             'vnvtvnxjrtffdhrfvczzoyeokjabxcilmmsrhwuakghvuabcmfpmblyroodmhfivmhqoiqhapoglwaluewhqkunzitmvijaictjdncivccedfpaezcnpwemlohbhjjlqsonuclaumgbzjamsrhuzqdqtitygggsnruuccdtxkgbdd',
             self::WIN_FIRST
-        ];
+        );
 
-        $data[19] = [
+        $data[19] = array(
             'hxueikegwnrctlciwguepdsgupguykrntbszeqzzbpdlouwnmqgzcxejidstxyxhdlnttnibxstduwiflouzfswfikdudkazoefawm',
             self::WIN_SECOND
-        ];
+        );
 
-        $data[20] = [
+        $data[20] = array(
             'aaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbccccccccccccccccccccddddddddddeeeeeeeeeeffffgggghhhhiiiijjjjqqqqwwwweeeerrrrttttyyyyuuuuiiiiooooppppaaaassssddddffffgggghhhhjjjjkkkkllllzzzzxxxxccccvvvvbbbbnnnnmmmm',
             self::WIN_FIRST
-        ];
+        );
 
         return $data;
     }
